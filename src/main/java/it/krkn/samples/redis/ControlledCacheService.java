1e3f455d07d521cadd438a1eb665fa1fbfc9b919
Matteo <matteo@MBP-di-Matteo.home-life.hub>
2020-05-05 09:07:22 +0200

    Initial commit

---
+++ src/main/java/it/krkn/samples/redis/ControlledCacheService.java
@@ -0,0 +1,24 @@
+package it.krkn.samples.redis;
+
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ControlledCacheService {
+
+    @Cacheable(cacheNames = "myControlledCache", key = "'myControlledPrefix_'.concat(#relevant)")
+    public String getFromCache(String relevant) {
+        return null;
+    }
+
+    @CacheEvict(cacheNames = "myControlledCache", key = "'myControlledPrefix_'.concat(#relevant)")
+    public void removeFromCache(String relevant) {
+    }
+
+    @CachePut(cacheNames = "myControlledCache", key = "'myControlledPrefix_'.concat(#relevant)")
+    public String populateCache(String relevant, String unrelevantTrackingId) {
+        return "this is it again!";
+    }
+}
---
+++ src/test/java/it/krkn/samples/redis/RedisApplicationTests.java
@@ -0,0 +1,13 @@
+package it.krkn.samples.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
